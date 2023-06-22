096a1cc5b197a1abc66a074ce04b6091849480d9
sendra <polsendra@gmail.com>
2023-06-22 12:26:37 +0200

    fix: added factory

--- src/contracts/AToken.sol
+++ src/contracts/AToken.sol
@@ -2,9 +2,9 @@
 pragma solidity ^0.8.10;
 
 import {IERC20} from 'aave-v3-core/contracts/dependencies/openzeppelin/contracts/IERC20.sol';
-import {GPv2SafeERC20} from 'aave-v3-core/contracts/dependencies/gnosis/contracts/GPv2SafeERC20.sol';
-import {SafeCast} from 'aave-v3-core/contracts/dependencies/openzeppelin/contracts/SafeCast.sol';
-import {VersionedInitializable} from 'aave-v3-core/contracts/protocol/libraries/aave-upgradeability/VersionedInitializable.sol';
+import {GPv2SafeERC20} from './dependencies/GPv2SafeERC20.sol';
+import {SafeCast} from './dependencies/SafeCast.sol';
+import {VersionedInitializable} from './dependencies/VersionedInitializable.sol';
 import {Errors} from 'aave-v3-core/contracts/protocol/libraries/helpers/Errors.sol';
 import {WadRayMath} from 'aave-v3-core/contracts/protocol/libraries/math/WadRayMath.sol';
 import {IPool} from 'aave-v3-core/contracts/interfaces/IPool.sol';
@@ -13,7 +13,8 @@ import {IAaveIncentivesController} from 'aave-v3-core/contracts/interfaces/IAave
 import {IInitializableAToken} from 'aave-v3-core/contracts/interfaces/IInitializableAToken.sol';
 import {ScaledBalanceTokenBase} from './ScaledBalanceTokenBase.sol';
 import {IncentivizedERC20} from './IncentivizedERC20.sol';
-import {EIP712Base} from 'aave-v3-core/contracts/protocol/tokenization/base/EIP712Base.sol';
+import {EIP712Base} from './dependencies/EIP712Base.sol';
+import {ECDSA} from 'openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol';
 
 /**
  * @title Aave ERC20 AToken
@@ -28,7 +29,7 @@ contract AToken is VersionedInitializable, ScaledBalanceTokenBase, EIP712Base, I
   bytes32 public constant PERMIT_TYPEHASH =
     keccak256('Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)');
 
-  uint256 public constant ATOKEN_REVISION = 0x1;
+  uint256 public constant ATOKEN_REVISION = 0x2;
 
   address internal _treasury;
   address internal _underlyingAsset;
@@ -44,7 +45,7 @@ contract AToken is VersionedInitializable, ScaledBalanceTokenBase, EIP712Base, I
    */
   constructor(
     IPool pool
-  ) ScaledBalanceTokenBase(pool, 'ATOKEN_IMPL', 'ATOKEN_IMPL', 0) EIP712Base() {
+  ) ScaledBalanceTokenBase(pool, 'Aave Ethereum AAVE', 'aEthAAVE', 0) EIP712Base() {
     // Intentionally left blank
   }
 
@@ -180,14 +181,10 @@ contract AToken is VersionedInitializable, ScaledBalanceTokenBase, EIP712Base, I
     //solium-disable-next-line
     require(block.timestamp <= deadline, Errors.INVALID_EXPIRATION);
     uint256 currentValidNonce = _nonces[owner];
-    bytes32 digest = keccak256(
-      abi.encodePacked(
-        '\x19\x01',
-        DOMAIN_SEPARATOR(),
-        keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, currentValidNonce, deadline))
-      )
+    bytes32 digest = _hashTypedDataV4(
+      keccak256(abi.encode(PERMIT_TYPEHASH, owner, spender, value, currentValidNonce, deadline))
     );
-    require(owner == ecrecover(digest, v, r, s), Errors.INVALID_SIGNATURE);
+    require(owner == ECDSA.recover(digest, v, r, s), Errors.INVALID_SIGNATURE);
     _nonces[owner] = currentValidNonce + 1;
     _approve(owner, spender, value);
   }
---
+++ src/contracts/dependencies/EIP712Base.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.10;
+
+import {EIP712, ECDSA} from 'openzeppelin-contracts/contracts/utils/cryptography/EIP712.sol';
+
+/**
+ * @title EIP712Base
+ * @author Aave
+ * @notice Base contract implementation of EIP712.
+ */
+abstract contract EIP712Base is EIP712 {
+  bytes public constant EIP712_REVISION = bytes('1');
+  bytes32 internal constant EIP712_DOMAIN =
+    keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)');
+
+  // Map of address nonces (address => nonce)
+  mapping(address => uint256) internal _nonces;
+
+  bytes32 internal _domainSeparator;
+  uint256 internal immutable _chainId;
+
+  /**
+   * @dev Constructor.
+   */
+  constructor() EIP712('Aave Ethereum AAVE', '49') {
+    _chainId = block.chainid;
+  }
+
+  /**
+   * @notice Get the domain separator for the token
+   * @dev Return cached value if chainId matches cache, otherwise recomputes separator
+   * @return The domain separator of the token at current chain
+   */
+  function DOMAIN_SEPARATOR() public view virtual returns (bytes32) {
+    return _domainSeparatorV4();
+  }
+
+  /**
+   * @notice Returns the nonce value for address specified as parameter
+   * @param owner The address for which the nonce is being returned
+   * @return The nonce value for the input address`
+   */
+  function nonces(address owner) public view virtual returns (uint256) {
+    return _nonces[owner];
+  }
+
+  /**
+   * @notice Returns the user readable name of signing domain (e.g. token name)
+   * @return The name of the signing domain
+   */
+  function _EIP712BaseId() internal view virtual returns (string memory);
+}
